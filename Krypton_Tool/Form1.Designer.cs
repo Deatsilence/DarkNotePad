320abc18a58eb3baf3cc5f393dc3391e75169704
Mert Doğan <mert_im2000@hotmail.com>
2022-06-26 16:49:15 +0300

    The Project started and maximized didn't do

--- Krypton_Tool/Form1.Designer.cs
+++ Krypton_Tool/Form1.Designer.cs
@@ -32,10 +32,11 @@ namespace Krypton_Tool
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotePadPage));
             this.kryptonPalette1 = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
-            this.rjBtnClose = new RJControls.RJConpanents.RJButton();
-            this.rjBtnMaximize = new RJControls.RJConpanents.RJButton();
             this.rjBtnHide = new RJControls.RJConpanents.RJButton();
-            this.kryptonRichTextBox1 = new ComponentFactory.Krypton.Toolkit.KryptonRichTextBox();
+            this.txtBoxKryptonText = new ComponentFactory.Krypton.Toolkit.KryptonRichTextBox();
+            this.rjBtnMaximize = new RJControls.RJConpanents.RJButton();
+            this.rjBtnClose = new RJControls.RJConpanents.RJButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.SuspendLayout();
             // 
             // kryptonPalette1
@@ -83,44 +84,11 @@ namespace Krypton_Tool
             this.kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.ButtonEdgeInset = 10;
             this.kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.Content.Padding = new System.Windows.Forms.Padding(10, -1, -1, -1);
             // 
-            // rjBtnClose
-            // 
-            this.rjBtnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
-            this.rjBtnClose.BackGroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
-            this.rjBtnClose.BorderColor = System.Drawing.Color.PaleVioletRed;
-            this.rjBtnClose.BorderRadius = 12;
-            this.rjBtnClose.BorderSize = 0;
-            this.rjBtnClose.FlatAppearance.BorderSize = 0;
-            this.rjBtnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.rjBtnClose.Font = new System.Drawing.Font("Poppins Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rjBtnClose.ForeColor = System.Drawing.Color.Black;
-            this.rjBtnClose.Location = new System.Drawing.Point(789, -2);
-            this.rjBtnClose.Name = "rjBtnClose";
-            this.rjBtnClose.Size = new System.Drawing.Size(46, 31);
-            this.rjBtnClose.TabIndex = 5;
-            this.rjBtnClose.TextColor = System.Drawing.Color.Black;
-            this.rjBtnClose.UseVisualStyleBackColor = false;
-            // 
-            // rjBtnMaximize
-            // 
-            this.rjBtnMaximize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
-            this.rjBtnMaximize.BackGroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
-            this.rjBtnMaximize.BorderColor = System.Drawing.Color.PaleVioletRed;
-            this.rjBtnMaximize.BorderRadius = 12;
-            this.rjBtnMaximize.BorderSize = 0;
-            this.rjBtnMaximize.FlatAppearance.BorderSize = 0;
-            this.rjBtnMaximize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.rjBtnMaximize.ForeColor = System.Drawing.Color.Black;
-            this.rjBtnMaximize.Image = ((System.Drawing.Image)(resources.GetObject("rjBtnMaximize.Image")));
-            this.rjBtnMaximize.Location = new System.Drawing.Point(743, -2);
-            this.rjBtnMaximize.Name = "rjBtnMaximize";
-            this.rjBtnMaximize.Size = new System.Drawing.Size(46, 31);
-            this.rjBtnMaximize.TabIndex = 6;
-            this.rjBtnMaximize.TextColor = System.Drawing.Color.Black;
-            this.rjBtnMaximize.UseVisualStyleBackColor = false;
-            // 
             // rjBtnHide
             // 
+            this.rjBtnHide.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.rjBtnHide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
             this.rjBtnHide.BackGroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
             this.rjBtnHide.BorderColor = System.Drawing.Color.PaleVioletRed;
@@ -136,34 +104,97 @@ namespace Krypton_Tool
             this.rjBtnHide.Text = "__";
             this.rjBtnHide.TextColor = System.Drawing.Color.Black;
             this.rjBtnHide.UseVisualStyleBackColor = false;
+            this.rjBtnHide.Click += new System.EventHandler(this.rjBtnHide_Click);
             // 
-            // kryptonRichTextBox1
+            // txtBoxKryptonText
             // 
-            this.kryptonRichTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.kryptonRichTextBox1.Location = new System.Drawing.Point(0, 35);
-            this.kryptonRichTextBox1.Name = "kryptonRichTextBox1";
-            this.kryptonRichTextBox1.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.ProfessionalSystem;
-            this.kryptonRichTextBox1.Size = new System.Drawing.Size(834, 426);
-            this.kryptonRichTextBox1.TabIndex = 8;
-            this.kryptonRichTextBox1.Text = "kryptonRichTextBox1";
+            this.txtBoxKryptonText.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtBoxKryptonText.Location = new System.Drawing.Point(0, 53);
+            this.txtBoxKryptonText.Name = "txtBoxKryptonText";
+            this.txtBoxKryptonText.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.ProfessionalSystem;
+            this.txtBoxKryptonText.Size = new System.Drawing.Size(834, 426);
+            this.txtBoxKryptonText.TabIndex = 8;
+            this.txtBoxKryptonText.Text = "kryptonRichTextBox1";
+            // 
+            // rjBtnMaximize
+            // 
+            this.rjBtnMaximize.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rjBtnMaximize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.rjBtnMaximize.BackGroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.rjBtnMaximize.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.rjBtnMaximize.BorderRadius = 12;
+            this.rjBtnMaximize.BorderSize = 0;
+            this.rjBtnMaximize.FlatAppearance.BorderSize = 0;
+            this.rjBtnMaximize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjBtnMaximize.ForeColor = System.Drawing.Color.Black;
+            this.rjBtnMaximize.Image = ((System.Drawing.Image)(resources.GetObject("rjBtnMaximize.Image")));
+            this.rjBtnMaximize.Location = new System.Drawing.Point(743, -2);
+            this.rjBtnMaximize.Name = "rjBtnMaximize";
+            this.rjBtnMaximize.Size = new System.Drawing.Size(46, 31);
+            this.rjBtnMaximize.TabIndex = 6;
+            this.rjBtnMaximize.TextColor = System.Drawing.Color.Black;
+            this.rjBtnMaximize.UseVisualStyleBackColor = false;
+            this.rjBtnMaximize.Click += new System.EventHandler(this.rjBtnMaximize_Click);
+            // 
+            // rjBtnClose
+            // 
+            this.rjBtnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rjBtnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.rjBtnClose.BackGroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.rjBtnClose.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.rjBtnClose.BorderRadius = 12;
+            this.rjBtnClose.BorderSize = 0;
+            this.rjBtnClose.FlatAppearance.BorderSize = 0;
+            this.rjBtnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjBtnClose.Font = new System.Drawing.Font("Poppins Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rjBtnClose.ForeColor = System.Drawing.Color.Black;
+            this.rjBtnClose.Image = ((System.Drawing.Image)(resources.GetObject("rjBtnClose.Image")));
+            this.rjBtnClose.Location = new System.Drawing.Point(789, -2);
+            this.rjBtnClose.Name = "rjBtnClose";
+            this.rjBtnClose.Size = new System.Drawing.Size(46, 31);
+            this.rjBtnClose.TabIndex = 5;
+            this.rjBtnClose.TextColor = System.Drawing.Color.Black;
+            this.rjBtnClose.UseVisualStyleBackColor = false;
+            this.rjBtnClose.Click += new System.EventHandler(this.rjBtnClose_Click);
+            this.rjBtnClose.MouseEnter += new System.EventHandler(this.rjBtnClose_MouseEnter);
+            this.rjBtnClose.MouseLeave += new System.EventHandler(this.rjBtnClose_MouseLeave);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AutoSize = false;
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 26);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(694, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
             // 
             // NotePadPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
-            this.ClientSize = new System.Drawing.Size(834, 461);
-            this.Controls.Add(this.kryptonRichTextBox1);
+            this.ClientSize = new System.Drawing.Size(834, 479);
+            this.Controls.Add(this.txtBoxKryptonText);
             this.Controls.Add(this.rjBtnHide);
             this.Controls.Add(this.rjBtnMaximize);
             this.Controls.Add(this.rjBtnClose);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "NotePadPage";
             this.Palette = this.kryptonPalette1;
             this.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "The Ultimate UI ToolKit";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.NotePadPage_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.NotePadPage_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.NotePadPage_MouseMove);
@@ -178,7 +209,8 @@ namespace Krypton_Tool
         private RJControls.RJConpanents.RJButton rjBtnClose;
         private RJControls.RJConpanents.RJButton rjBtnMaximize;
         private RJControls.RJConpanents.RJButton rjBtnHide;
-        private ComponentFactory.Krypton.Toolkit.KryptonRichTextBox kryptonRichTextBox1;
+        private ComponentFactory.Krypton.Toolkit.KryptonRichTextBox txtBoxKryptonText;
+        private System.Windows.Forms.MenuStrip menuStrip1;
     }
 }
 
--- Krypton_Tool/Form1.cs
+++ Krypton_Tool/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using ComponentFactory.Krypton.Toolkit;
 using RJControls.RJConpanents;
 using RJControls;
+using System.Runtime.InteropServices;
 
 namespace Krypton_Tool
 {
@@ -18,22 +19,23 @@ namespace Krypton_Tool
         // For the movement of the form
         bool formMoving = false;
         Point startPoint = new Point(0, 0);
+        string strMyOriginalText;
 
 
         // Constructor
         public NotePadPage()
         {
             InitializeComponent();
+            strMyOriginalText = txtBoxKryptonText.Text;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // this
+            this.MinimumSize = this.Size;
+
             // Form Buttons
             rjBtnClose.FlatAppearance.MouseOverBackColor = Color.Red;
-            if (rjBtnClose.FlatAppearance.MouseDownBackColor == Color.Red)
-                rjBtnClose.ForeColor = Color.FromArgb(250, 252, 252);
-            else
-                rjBtnClose.ForeColor = Color.FromArgb(250, 252, 252);
         }
 
         private void NotePadPage_MouseDown(object sender, MouseEventArgs e)
@@ -54,6 +56,70 @@ namespace Krypton_Tool
                 Point p = PointToScreen(e.Location);
                 Location = new Point(p.X - this.startPoint.X, p.Y - this.startPoint.Y);
             }
+        }
+
+        private void rjBtnClose_MouseEnter(object sender, EventArgs e)
+        {
+            rjBtnClose.Image = Properties.Resources.closewhite;
+        }
+
+        private void rjBtnClose_MouseLeave(object sender, EventArgs e)
+        {
+            rjBtnClose.Image = Properties.Resources.closedark;
+        }
+
+        private void rjBtnClose_Click(object sender, EventArgs e)
+        {
+
+            Application.Exit();
+        }
+
+        private void NotePadPage_FormClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // Determine if text has changed in the textbox by comparing to original text.
+            if (txtBoxKryptonText.Text != strMyOriginalText)
+            {
+                // Display a MsgBox asking the user to save changes or abort.
+                if (MessageBox.Show("Kaydetmeden Çıkmak İstediğinize Emin misiniz?", "Uyarı",
+                   MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                {
+                    // Cancel the Closing event from closing the form.
+                    e.Cancel = true;
+                    // Call method to save file...
+                }
+            }
+        }
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+
+        private void rjBtnMaximize_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+                Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 0, 0));
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+                Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 30, 30));
+            }
+
+            
+
+        }
+
+        private void rjBtnHide_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
         }
     }
 }
