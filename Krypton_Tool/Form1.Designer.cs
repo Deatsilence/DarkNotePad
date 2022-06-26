ec023e5ebf6f9c727f9cd78d95d48fe0c1efba72
Mert Doğan <mert_im2000@hotmail.com>
2022-06-26 15:07:29 +0300

    the project started

---
+++ Krypton_Tool/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace Krypton_Tool
+{
+    partial class NotePadPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotePadPage));
+            this.kryptonPalette1 = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
+            this.rjBtnClose = new RJControls.RJConpanents.RJButton();
+            this.rjBtnMaximize = new RJControls.RJConpanents.RJButton();
+            this.rjBtnHide = new RJControls.RJConpanents.RJButton();
+            this.kryptonRichTextBox1 = new ComponentFactory.Krypton.Toolkit.KryptonRichTextBox();
+            this.SuspendLayout();
+            // 
+            // kryptonPalette1
+            // 
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateNormal.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateNormal.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateNormal.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateNormal.Border.Width = 0;
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StatePressed.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StatePressed.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StatePressed.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StatePressed.Border.Width = 0;
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateTracking.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateTracking.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateTracking.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateTracking.Border.Width = 0;
+            this.kryptonPalette1.ButtonStyles.ButtonFormClose.StatePressed.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.ButtonStyles.ButtonFormClose.StatePressed.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.ButtonStyles.ButtonFormClose.StatePressed.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPalette1.ButtonStyles.ButtonFormClose.StatePressed.Border.Width = 0;
+            this.kryptonPalette1.ButtonStyles.ButtonFormClose.StateTracking.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.ButtonStyles.ButtonFormClose.StateTracking.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.ButtonStyles.ButtonFormClose.StateTracking.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPalette1.ButtonStyles.ButtonFormClose.StateTracking.Border.Width = 0;
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.None;
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.Rounding = 12;
+            this.kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.ButtonEdgeInset = 10;
+            this.kryptonPalette1.HeaderStyles.HeaderForm.StateCommon.Content.Padding = new System.Windows.Forms.Padding(10, -1, -1, -1);
+            // 
+            // rjBtnClose
+            // 
+            this.rjBtnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.rjBtnClose.BackGroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.rjBtnClose.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.rjBtnClose.BorderRadius = 12;
+            this.rjBtnClose.BorderSize = 0;
+            this.rjBtnClose.FlatAppearance.BorderSize = 0;
+            this.rjBtnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjBtnClose.Font = new System.Drawing.Font("Poppins Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rjBtnClose.ForeColor = System.Drawing.Color.Black;
+            this.rjBtnClose.Location = new System.Drawing.Point(789, -2);
+            this.rjBtnClose.Name = "rjBtnClose";
+            this.rjBtnClose.Size = new System.Drawing.Size(46, 31);
+            this.rjBtnClose.TabIndex = 5;
+            this.rjBtnClose.TextColor = System.Drawing.Color.Black;
+            this.rjBtnClose.UseVisualStyleBackColor = false;
+            // 
+            // rjBtnMaximize
+            // 
+            this.rjBtnMaximize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.rjBtnMaximize.BackGroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.rjBtnMaximize.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.rjBtnMaximize.BorderRadius = 12;
+            this.rjBtnMaximize.BorderSize = 0;
+            this.rjBtnMaximize.FlatAppearance.BorderSize = 0;
+            this.rjBtnMaximize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjBtnMaximize.ForeColor = System.Drawing.Color.Black;
+            this.rjBtnMaximize.Image = ((System.Drawing.Image)(resources.GetObject("rjBtnMaximize.Image")));
+            this.rjBtnMaximize.Location = new System.Drawing.Point(743, -2);
+            this.rjBtnMaximize.Name = "rjBtnMaximize";
+            this.rjBtnMaximize.Size = new System.Drawing.Size(46, 31);
+            this.rjBtnMaximize.TabIndex = 6;
+            this.rjBtnMaximize.TextColor = System.Drawing.Color.Black;
+            this.rjBtnMaximize.UseVisualStyleBackColor = false;
+            // 
+            // rjBtnHide
+            // 
+            this.rjBtnHide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.rjBtnHide.BackGroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.rjBtnHide.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.rjBtnHide.BorderRadius = 12;
+            this.rjBtnHide.BorderSize = 0;
+            this.rjBtnHide.FlatAppearance.BorderSize = 0;
+            this.rjBtnHide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjBtnHide.ForeColor = System.Drawing.Color.Black;
+            this.rjBtnHide.Location = new System.Drawing.Point(697, -2);
+            this.rjBtnHide.Name = "rjBtnHide";
+            this.rjBtnHide.Size = new System.Drawing.Size(46, 31);
+            this.rjBtnHide.TabIndex = 7;
+            this.rjBtnHide.Text = "__";
+            this.rjBtnHide.TextColor = System.Drawing.Color.Black;
+            this.rjBtnHide.UseVisualStyleBackColor = false;
+            // 
+            // kryptonRichTextBox1
+            // 
+            this.kryptonRichTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.kryptonRichTextBox1.Location = new System.Drawing.Point(0, 35);
+            this.kryptonRichTextBox1.Name = "kryptonRichTextBox1";
+            this.kryptonRichTextBox1.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.ProfessionalSystem;
+            this.kryptonRichTextBox1.Size = new System.Drawing.Size(834, 426);
+            this.kryptonRichTextBox1.TabIndex = 8;
+            this.kryptonRichTextBox1.Text = "kryptonRichTextBox1";
+            // 
+            // NotePadPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.ClientSize = new System.Drawing.Size(834, 461);
+            this.Controls.Add(this.kryptonRichTextBox1);
+            this.Controls.Add(this.rjBtnHide);
+            this.Controls.Add(this.rjBtnMaximize);
+            this.Controls.Add(this.rjBtnClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "NotePadPage";
+            this.Palette = this.kryptonPalette1;
+            this.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "The Ultimate UI ToolKit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.NotePadPage_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.NotePadPage_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.NotePadPage_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPalette kryptonPalette1;
+        private RJControls.RJConpanents.RJButton rjBtnClose;
+        private RJControls.RJConpanents.RJButton rjBtnMaximize;
+        private RJControls.RJConpanents.RJButton rjBtnHide;
+        private ComponentFactory.Krypton.Toolkit.KryptonRichTextBox kryptonRichTextBox1;
+    }
+}
+
---
+++ Krypton_Tool/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+using RJControls.RJConpanents;
+using RJControls;
+
+namespace Krypton_Tool
+{
+    public partial class NotePadPage : KryptonForm
+    {
+        // For the movement of the form
+        bool formMoving = false;
+        Point startPoint = new Point(0, 0);
+
+
+        // Constructor
+        public NotePadPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Form Buttons
+            rjBtnClose.FlatAppearance.MouseOverBackColor = Color.Red;
+            if (rjBtnClose.FlatAppearance.MouseDownBackColor == Color.Red)
+                rjBtnClose.ForeColor = Color.FromArgb(250, 252, 252);
+            else
+                rjBtnClose.ForeColor = Color.FromArgb(250, 252, 252);
+        }
+
+        private void NotePadPage_MouseDown(object sender, MouseEventArgs e)
+        {
+            formMoving = true;
+            startPoint = new Point(e.X, e.Y);
+        }
+
+        private void NotePadPage_MouseUp(object sender, MouseEventArgs e)
+        {
+            formMoving = false;
+        }
+
+        private void NotePadPage_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (formMoving)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this.startPoint.X, p.Y - this.startPoint.Y);
+            }
+        }
+    }
+}
